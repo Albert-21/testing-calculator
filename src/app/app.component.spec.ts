4d361c591a482d61ef931f4286c31f0650a2adaa
Albert-210 <74438153+Albert-210@users.noreply.github.com>
2020-11-15 20:17:56 -0600

    Revert "Closes #4"

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -28,5 +28,4 @@ describe('AppComponent', () => {
     const compiled = fixture.nativeElement;
     expect(compiled.querySelector('.content span').textContent).toContain('testing-calculator app is running!');
   });
-  
 });
--- src/app/division/division.ts
+++
@@ -1,5 +0,0 @@
-export function division(oper1,oper2){
-    let div = 0;
-    div = oper1/oper2;
-    return div;
-    }
