ab9dc21d3988a43496ee536faaafa83c8ef10697
Albert-21 <alberto.ah14@hotmail.com>
2020-11-16 19:39:09 -0600

    addition ui, test #26

--- src/app/ui/ui.component.spec.ts
+++ src/app/ui/ui.component.spec.ts
@@ -1,40 +1,19 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { UiComponent } from './ui.component';
-
-describe('UiComponent', () => {
-  let component: UiComponent;
-  let fixture: ComponentFixture<UiComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ UiComponent ]
-    })
-    .compileComponents();
-  });
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(UiComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
-
+import { FormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 
 describe('Ui Addition - Component', () => {
   let component: UiComponent;
   let fixture: ComponentFixture<UiComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ UiComponent ]
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ UiComponent ],
+      imports: [FormsModule],
     })
     .compileComponents();
-  });
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(UiComponent);
@@ -42,167 +21,82 @@ describe('Ui Addition - Component', () => {
     fixture.detectChanges();
   });
 
-  it('Should add call addition method', () => {
-    expect(component).toBeFalsy();
+  it('Should call addition method', () => {
+     // Arrange
+     let result = 0;
+     component.operator1 = 2;
+     component.operator2 = 2;
+ 
+     // Act
+     component.addition();
+     result = component.result;
+ 
+     // Assert
+     expect(result).toBe(4);
   });
 
-  it('Should render sum in TxtResult', () => {
-    expect(component).toBeFalsy();
-  });
-});
 
-describe('Ui Substraction - Component', () => {
-  let component: UiComponent;
-  let fixture: ComponentFixture<UiComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ UiComponent ]
-    })
-    .compileComponents();
-  });
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(UiComponent);
-    component = fixture.componentInstance;
+  it('Should set operator1 model through ngModel', async() => {
+    // Arrange 
+    await fixture.whenStable();
     fixture.detectChanges();
-  });
+    const inputElement = fixture.debugElement.query(By.css('input[name="operator1"]')).nativeElement;
 
-  it('Should add call substraction method', () => {
-    expect(component).toBeFalsy();
-  });
-
-  it('Should render subs in TxtResult', () => {
-    expect(component).toBeFalsy();
-  });
-});
-
-describe('Ui Multiplication - Component', () => {
-  let component: UiComponent;
-  let fixture: ComponentFixture<UiComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ UiComponent ]
-    })
-    .compileComponents();
-  });
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(UiComponent);
-    component = fixture.componentInstance;
+    // Act 
+    inputElement.value = '3.1416';
+    inputElement.dispatchEvent(new Event('input'));
     fixture.detectChanges();
+
+    // Assert 
+    expect(component.operator1).toEqual(3.1416);
   });
-
-  it('Should add call multiplication method', () => {
-    expect(component).toBeFalsy();
-  });
-
-  it('Should render mult in TxtResult', () => {
-    expect(component).toBeFalsy();
-  });
-});
-
-describe('Ui Division - Component', () => {
-  let component: UiComponent;
-  let fixture: ComponentFixture<UiComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ UiComponent ]
-    })
-    .compileComponents();
-  });
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(UiComponent);
-    component = fixture.componentInstance;
+ 
+  it('Should set operator2 model through ngModel', async() => {
+    // Arrange 
+    await fixture.whenStable();
     fixture.detectChanges();
-  });
+    const inputElement = fixture.debugElement.query(By.css('input[name="operator2"]')).nativeElement;
 
-  it('Should add call division method', () => {
-    expect(component).toBeFalsy();
-  });
-
-  it('Should render div in TxtResult', () => {
-    expect(component).toBeFalsy();
-  });
-});
-
-describe('Ui Exp - Component', () => {
-  let component: UiComponent;
-  let fixture: ComponentFixture<UiComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ UiComponent ]
-    })
-    .compileComponents();
-  });
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(UiComponent);
-    component = fixture.componentInstance;
+    // Act 
+    inputElement.value = '2.71';
+    inputElement.dispatchEvent(new Event('input'));
     fixture.detectChanges();
+
+    // Assert 
+    expect(component.operator2).toEqual(2.71);
   });
 
-  it('Should add call exp method', () => {
-    expect(component).toBeFalsy();
-  });
 
-  it('Should render exp in TxtResult', () => {
-    expect(component).toBeFalsy();
-  });
-});
+  it('should add operator1 and operator2 when i click the addition button ', () => {
+    // Arrange 
+    component.operator1 = 5.0;
+    component.operator2 = 2.5;
+    let additionButton = fixture.debugElement.query(By.css('.addition-button'));
 
-describe('Ui Sqr - Component', () => {
-  let component: UiComponent;
-  let fixture: ComponentFixture<UiComponent>;
+    // Act
+    additionButton.triggerEventHandler('click', null);
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ UiComponent ]
-    })
-    .compileComponents();
-  });
+    // Assert
+    expect(component.result).toBe(7.5);
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(UiComponent);
-    component = fixture.componentInstance;
+   });
+
+  it('Should render sum in result div', () => {
+    // Arrange
+    component.operator1 = 5;
+    component.operator2 = 5;
+ 
+    // Act
+    component.addition();
     fixture.detectChanges();
+    
+    let de = fixture.debugElement.query(By.css('.result'));
+    let el : HTMLElement = de.nativeElement;
+
+    // Assert
+    expect(el.innerText).toContain('10');
+     
   });
 
-  it('Should add call Sqr method', () => {
-    expect(component).toBeFalsy();
-  });
-
-  it('Should render Sqr in TxtResult', () => {
-    expect(component).toBeFalsy();
-  });
-});
-
-describe('Ui Sqrt - Component', () => {
-  let component: UiComponent;
-  let fixture: ComponentFixture<UiComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ UiComponent ]
-    })
-    .compileComponents();
-  });
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(UiComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('Should add call Sqrt method', () => {
-    expect(component).toBeFalsy();
-  });
-
-  it('Should render Sqrt in TxtResult', () => {
-    expect(component).toBeFalsy();
-  });
 });
