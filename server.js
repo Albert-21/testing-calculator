18cd53d505e32cb8439bd8c61d6812af32f882d6
Albert-21 <alberto.ah14@hotmail.com>
2020-12-09 11:52:45 -0600

    update

--- server.js
+++ server.js
@@ -11,4 +11,4 @@ app.get('/*', function(req, res) {
 });
 
 // default Heroku PORT
-app.listen(process.env.PORT || 3000);
+http.listen((process.env.PORT || 5000), function(){ console.log('listening on *:5000'); });
