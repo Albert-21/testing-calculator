5c343225712d864891e05c57f0a6f8b0cab1932a
Albert-21 <alberto.ah14@hotmail.com>
2020-12-10 15:57:09 -0600

    se corrigieron errores de heroku

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require('express');
 const path = require('path');
+const express = require('express');
 const app = express();
 
-// Serve static files....
+// Serve static files
 app.use(express.static(__dirname + '/dist/testing-calculator'));
 
 // Send all requests to index.html
@@ -10,5 +10,5 @@ app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/testing-calculator/index.html'));
 });
 
-// default Heroku PORT
-http.listen((process.env.PORT || 5000), function(){ console.log('listening on *:5000'); });
+// default Heroku port
+app.listen(process.env.PORT || 5000);
